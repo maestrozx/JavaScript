2dc0c81f41504682292a640f052985810aabd6ce
Liberius <liberiusongere@gmail.com>
2023-05-25 16:45:30 +0300

    first commit

---
+++ Loop.js
@@ -0,0 +1,23 @@
+//For loop
+for(let num =1; num <= 5; num++){
+    console.log(num);
+}
+
+console.log("Counting completed");
+
+//While loop
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+};
+console.log('Counting completed!');
+
+//if Loop
+let num = 5;
+if(num % 2 == 0){
+    console.log(`The number ${num} is even.`);
+}
+else {
+    console.log(`The number ${num} is odd.`);
+}
