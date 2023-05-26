f7349ab594801fa553936df2dffd9459bc24151e
Liberius <liberiusongere@gmail.com>
2023-05-26 22:08:58 +0300

    Practice

---
+++ Array.js
@@ -0,0 +1,15 @@
+/*
+let clothes = [];
+clothes.push('abacus', 'lingerie');
+console.log(clothes[0]);
+*/
+
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.pop();
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.push('green scarf');
+console.log(clothes);
